dc6dd545ddc77ccb5b89ef0997ce95cd6038e533
Kai Cheung <kaicheunga@gmail.com>
2025-03-18 00:50:22 +0000

    Fix animation for matched pairs and add rest of images

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const shuffle = (array) => {
   return array;
 };
 
+const small = 3
+const medium = 6
+const large = 10
+const massive = 15
+
 // limit to 3 images for now, can expand later
 const cardsArray = [
   {
@@ -24,6 +29,57 @@ const cardsArray = [
   },
 ];
 
+const cardsArrayMed = [
+  {
+    name: "orange",
+    img: "images/Orange.png"
+  },
+  {
+    name: "avocado",
+    img: "images/Avocado.png",
+  },
+  {
+    name: "banana",
+    img: "images/Banana.png",
+  },
+  {
+    name: "blueberry",
+    img: "images/Blueberry.png",
+  },
+  {
+    name: "coconut",
+    img: "images/Coconut.png",
+  },
+  {
+    name: "dragonfruit",
+    img: "images/Dragonfruit.png",
+  },
+  {
+    name: "durian",
+    img: "images/Durian.png",
+  },
+  {
+    name: "grape",
+    img: "images/Grape.png",
+  },
+  {
+    name: "mango",
+    img: "images/Mango.png",
+  },
+  {
+    name: "mangosteen",
+    img: "images/Mangosteen.png",
+  },
+  {
+    name: "papaya",
+    img: "images/Papaya.png",
+  },
+  {
+    name: "peach",
+    img: "images/Peach.png",
+  }
+];
+
 const gameGrid = cardsArray.concat(cardsArray);
 
 shuffle(gameGrid);
@@ -34,22 +90,34 @@ gameGrid.forEach((item) => {
   const card = document.createElement("div");
   card.classList.add("card");
   card.dataset.name = item.name;
-  card.style.backgroundImage = `url(${item.img})`;
+
+  const cover = document.createElement("div");
+  cover.classList.add("cover");
+
+  const face = document.createElement("div");
+  face.classList.add("face");
+  face.style.backgroundImage = `url(${item.img})`;
+
   grid.appendChild(card);
+  card.appendChild(cover);
+  card.appendChild(face);
 });
 
 let count = 0;
 let firstGuess = "";
 let secondGuess = "";
+const timeout = 1200;
 
 const isMatch = (firstGuess, secondGuess) => {
   count = 0;
-  firstGuess.classList.remove("selected");
-  secondGuess.classList.remove("selected");
   if (firstGuess.dataset.name === secondGuess.dataset.name) {
     firstGuess.classList.add("match");
     secondGuess.classList.add("match");
   }
+  setTimeout(() => {
+    firstGuess.classList.remove("selected");
+    secondGuess.classList.remove("selected");
+  }, timeout);
 };
 
 const clickCardLogic = (card) => {
@@ -73,9 +141,9 @@ const clickCardLogic = (card) => {
 };
 
 playArea.addEventListener("click", (e) => {
-  const clicked = e.target;
+  const clicked = e.target.closest(".card");
 
-  if (clicked.nodeName === "DIV" && !clicked.classList.contains("match")) {
+  if (clicked && clicked.nodeName === "DIV" && !clicked.classList.contains("match")) {
     clickCardLogic(clicked);
   }
 });
